5f358e5d956d2224e20b25b4409a4f9969cd02a3
maxjohn81 <rivoandriharisoa@gmail.com>
2026-06-20 23:54:46 +0300

    oui

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,28 +4,28 @@ const { loginService, logoutService } = require("../services/auth.service")
 const { success, error } = require("../utils/response.utils")
 
 const login = async (req, res) => {
-       try{
+        try {
                 const { email, password } = req.body; // les donnés utile
-                if(!email || !password) res.status(500).json({message:"les champs sont requis"}); //verification champ vide
+                if (!email || !password) res.status(500).json({ message: "les champs sont requis" }); //verification champ vide
 
 
-                const connected =  await loginService(req.body)
+                const connected = await loginService(req.body)
                 return success(res, connected, "vous etes connecter", 200);
-       } catch(error){
-                return res.status(500).json({error: error.message})
-       }
-  
+        } catch (error) {
+                return res.status(500).json({ error: error.message })
+        }
+
 };
 
-const logOut = async (req,res)=>{
+const logOut = async (req, res) => {
         try {
                 const userId = req.user.id // recupere depuis middleware
 
                 await logoutService(userId)
-                res.status(200).json({message: "Deconnexion reussi"})
+                res.status(200).json({ message: "Deconnexion reussi" })
 
         } catch (error) {
                 throw new Error(error.message)
         }
 }
-module.exports = {login, logOut}
+module.exports = { login, logOut }
